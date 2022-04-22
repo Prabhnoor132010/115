aead7d209a6954676e5332149cd87470e43bef9a
Prabhnoor132010 <79254675+Prabhnoor132010@users.noreply.github.com>
2022-04-22 17:12:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function preload() {
+    
+}
+
+function setup() {
+    canvas = createCanvas(400,300);
+    canvas.center();
+    video=createCapture(VIDEO)  ;    
+     video.size(400,300);   
+       video.hide();   
+         poseNet = ml5.poseNet(video, modelLoaded);  
+            poseNet.on('pose',gotPoses) 
+        } 
+
+
+
+    function gotPoses(results){  
+        if(results.length   > 0){   
+        console.log(results);
+        console.log("nose x = " +results[0].pose.nose.x);         
+        console.log("nose y = " +results[0].pose.nose.y);    
+     } 
+    } 
+
+
+
+      function modelLoaded() {     
+        console.log("Posenet is lod");
+    
+}
+
+function draw() {
+    image(video,0,0,400,300);
+}
+
+function take_snapshot() {
+ save('my filterImage.png');
+}
